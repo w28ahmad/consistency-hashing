913935eec322706b350fbc88f60e541695f79e6c
Wahab Ahmad <wahab.ahmad@streetcontxt.com>
2023-08-15 18:37:17 -0400

    Adding hashring

--- src/HashRing.py
+++ src/HashRing.py
@@ -1,4 +1,16 @@
+from src.Tree import AVLTree
+from hashlib import sha256
+
 
 class HashRing:
     def __init__(self):
+        self.hashring = AVLTree()
+
+    def hash(self, key):
+        hash = sha256(key.encode())
+        return int(hash.hexdigest(), 16)
+
+    def insert(self, key, data):
         pass
+
+
---
+++ tests/test_HashRing.py
@@ -0,0 +1,9 @@
+from src.HashRing import HashRing
+
+
+def runTests():
+    pass
+
+
+if __name__ == "__main__":
+    pass
