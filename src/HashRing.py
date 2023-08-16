471e846a3bd1297f9491891504259d93e6c1719b
Wahab Ahmad <w28ahmad@uwaterloo.ca>
2023-08-15 21:50:36 -0400

    Creating template

--- src/HashRing.py
+++ src/HashRing.py
@@ -6,11 +6,43 @@ class HashRing:
     def __init__(self):
         self.hashring = AVLTree()
 
-    def hash(self, key):
+    '''
+    @param key: String
+    @returns Int
+    '''
+
+    def hashInt(self, key):
         hash = sha256(key.encode())
         return int(hash.hexdigest(), 16)
 
-    def insert(self, key, data):
+    '''
+    @param nodeId: String
+    @returns Node
+    '''
+
+    def addNode(self, nodeId):
         pass
 
+    '''
+    @param nodeId: String
+    @returns None
+    '''
 
+    def removeNode(self, nodeId):
+        pass
+
+    '''
+    @param key: String
+    @returns None
+    '''
+
+    def addData(self, key, data):
+        pass
+
+    '''
+    @param key: String
+    @returns None
+    '''
+
+    def getData(self, key):
+        pass
